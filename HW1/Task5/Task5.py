8d58daac3f84519cf726ff7f4cb731e9edbaad6f
Julia Swan <julie112397@gmail.com>
2022-12-15 18:46:03 +0500

    Task5 some changes added

--- HW1/Task5/Task5.py
+++ HW1/Task5/Task5.py
@@ -1,6 +1,4 @@
 import math
-x1 = int(input('Введите x первой координаты: '))
-y1 = int(input('Введите y первой координаты: '))
-x2 = int(input('Введите x второй координаты: '))
-y2 = int(input('Введите y второй координаты: '))
+x1, y1 = int(input('Введите координаты первой точки через Enter: ')), int(input())
+x2, y2 = int(input('Введите координаты второй точки через Enter: ')), int(input())
 print(round(math.sqrt((x2-x1)**2 + (y2-y1)**2), 4))
